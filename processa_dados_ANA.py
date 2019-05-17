c5dd9214f16687cd4bb37f16b3ede422b141158b
alinedefreitasocn <alinedefreitasocn@users.noreply.github.com>
2019-05-17 13:33:41 -0300

    Add files via upload

--- processa_dados_ANA.py
+++ processa_dados_ANA.py
@@ -3,6 +3,20 @@
 Created on Fri May 17 08:14:06 2019
 
 @author: aline
+
+Em breve será um script de processamento total dos dados da ANA
+Até agora (17/05/2018) consegue:
+    1. Reescrever os arquivos da ANA: passar do formato original de tabela para
+    o formato de série temporal com a opção de escolher usar os dados brutos ou
+    os consistidos pela própria ANA (ask_consiste.ask). Gera arquivos CSV com
+    nome "serie_temporal" com apenas duas colunas, data e volume de chuva.
+
+    2. Gerar as tabelas de máximos valores mensais, fornecendo como saida um
+    arquivo em csv com colunas de ano inicial e final, o valor maximo de chuva
+    e a data de ocorrencia do evento. Utiliza o padrão de ano hidrologico indo
+    de 01/10/ano ate 30/09/ano+1
+        adiciona um limite de 350 registros dentro do ano hidrologico para
+        determinar o valor maximo de pluviosidade.
 """
 
 import pandas as pd
@@ -17,14 +31,13 @@ from reescrevendo_ANA import reescreve
 from ask_consiste import ask
 from find_nth import find_nth
 
-path = sg.PopupGetFolder('Pasta com arquivos da ANA a serem trabalhados')
+path = sg.PopupGetFolder('Pasta com arquivos a serem trabalhados')
 
 " ************************************************************************** "
 "                                                                            "
 "                 1.  REESCREVER OS ARQUIVOS DA ANA                          "
 "                                                                            "
 " ************************************************************************** "
-
 
 listfiles = glob.glob(path + '/*.csv', recursive=True)
 
@@ -34,7 +47,10 @@ current_value = 0
 for file in listfiles:
     current_value += 1
     max_value = len(listfiles)
-    w = sg.OneLineProgressMeter('Reescrevendo...', current_value, max_value, 'key')
+    w = sg.OneLineProgressMeter('Reescrevendo...',
+                                current_value,
+                                max_value,
+                                'key')
     reescreve(file, nconsiste)
     current_value += 1
 
