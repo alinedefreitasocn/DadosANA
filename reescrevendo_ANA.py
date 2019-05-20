635974d61e90eeb8fd2e4666490324038c8b48d1
alinedefreitasocn <alinedefreitasocn@users.noreply.github.com>
2019-05-20 10:27:25 -0300

    Update reescrevendo_ANA.py
    versao comentada

--- reescrevendo_ANA.py
+++ reescrevendo_ANA.py
@@ -6,15 +6,15 @@ Created on Tue Apr 09 10:05:09 2019
 @date: 2019-04-09
 
 
-
 ROTINA PARA LER O ARQUIVO DA ANA REESCREVENDO PARA UM SERIE TEMPORAL
 COM OS REGISTROS DIARIOS DE CHUVA
 
-PODEMOS DEPOIS TRANSFORMAR ISSO NUMA FUNCAO
+MICHEL RESOLVEU A QUESTAO DO ANO BISSEXTO CRIANDO A FUNCAO ANO_BISSEXTO
 
-
-ATENCAO!! ESTA PULANDO OS DIAS 29 DE FEVEREIRO PARA ANOS BISSEXTOS. ISSO
-AINDA PRECISA SER AJUSTADO, MAS NAO SEI MUITO BEM COMO.
+AGORA EH UMA FUNCAO QUE TEM COMO ENTRADA O NOME DO ARQUIVO E O 
+NCONSISTE, QUE EH O VALOR DE SAIDA DA FUNCAO ASK_CONSISTE
+DIZENDO SE VAI PEGAR OS DADOS CONSISTIDOS OU OS BRUTOS
+DA SERIE DA ANA
 
 
 
@@ -25,8 +25,7 @@ import numpy as np
 import PySimpleGUI as sg
 import os
 
-" ALTERAR SOMENTE ESSA LINHA COM O PATHNAME E O NOME DO ARQUIVO A SER LIDO "
-" SEPARADOS POR VIRGULA E COM A ULTIMA CONTRABARRA NA VARIAVEL DO FILENAME "
+
 def ano_bissexto(ano):
     if ano % 4 == 0:
         if ano % 100 != 0:
@@ -35,7 +34,6 @@ def ano_bissexto(ano):
     return False
 
 def reescreve(file, nconsiste):
-#    file = sg.PopupGetFile('Arquivo ANA original')
     path, filename = os.path.split(file)
     "Lendo arquivo da ANA"
     ANA = pd.read_csv(file,
@@ -46,7 +44,7 @@ def reescreve(file, nconsiste):
                       skiprows=12,
                       parse_dates=[2],
                       index_col=2)
-
+    
     # Dados da ANA estavam com entradas duplicadas...Uma linha eh dado
     # consistido e o outro eh bruto. Selecionar as linhas dos brutos
     # pq tem meses faltando nos consistidos.
@@ -54,58 +52,55 @@ def reescreve(file, nconsiste):
     # Selecionando apenas os valores consistidos, quando existentes
     # Nive consistencia == 1 == Dado Bruto
     # Nivel consistencia == 2 == Consistido
-
     " funcao ask() so abre um GUI perguntando se deseja usar"
     " os dados brutos ou consistidos "
-
+    
     ANA_consist = ANA[ANA.NivelConsistencia == nconsiste]
-    # completando com os dados brutos nos registros onde nao houve
-    # consistencia
-    #ANA_bruto = ANA[ANA.index > '2006-10-01']
-    #ANA = ANA_consist.append(ANA_bruto)
-
-
-    " Cria uma nova variavel soh com as colunas de chuva diaria "
-    "                chamada chuva_diaria                       "
-    #Contruindo uma lista de nome das colunas de chuva
+    
+    # Contruindo uma lista de nome das colunas de chuva
     list_nome_col = []
+    # AS COLUNAS DO ARQUIVO SAO NOMEADAS COMO CHUVA+DIA
+    # ONDE DIA VARIA DE 1 A 31
+    # pequeno loop para do caso dos dias menores que 10
+    # adiciona um zero a esquerda
     for i in np.arange(1, 32):
         if i < 10:
             nome_col = ('Chuva' + '0' + str(i))
         else:
             nome_col = ('Chuva' + str(i))
         list_nome_col.append(nome_col)
-
+    
+    # seleciona so as colunas que apresentam os valores de chuva
+    # descartando as demais. copia para um novo df
     chuva_diaria = ANA_consist[list_nome_col].copy()
-    #chuva_diaria = chuva_diaria[chuva_diaria > 0.2]
-    #
-    #chuva_diaria_media = np.array(chuva_diaria.groupby(
-    #        by=chuva_diaria.index.month).mean())
-
+    
     ANA_serie_temporal = pd.DataFrame(columns=['datahora', 'chuva'])
-    " Le linha por linha atraves das colunas de dia "
-    #diasdechuva = []
+    
+    # percorre cada uma das linhas do arquivo atraves do indice
+    # depois percorre cada uma das colunas naquela linha
+    # pegando assim o valor do acumulado para aquele dia
+    # e depois montando um novo datetime a partir da combinacao
+    # linha indice vs coluna header
+    " { Pega a linha toda a partir do indice de data} "
     for line in chuva_diaria.index:
-    #    b = [0]      # sempre zerar
-    #    chuva = []  # sempre zerar
-        " { Pega a linha toda a partir do indice de data} "
-    #    chuva = chuva_diaria[chuva_diaria.index == line]
         " { Percorre cada uma das colunas } "
         for header in list_nome_col:
     #        dia = int(header[-2:])  # pega so o numero do dia. i eh o nome da col
     #        mes = line.month
     #        ano = line.year
-
+            # faz uns testes para evitar que ele tente pegar o valor do dia 31
+            # de um mes que vai soh ate 30 e solucionando o problema do ano
+            # bissexto nos meses de fevereiro
             if int(header[-2:]) == 31 and line.month in [4, 6, 9, 11]:
                 continue
-
             elif line.month == 2 and int(header[-2:]) in range(29, 32) and not(bissexto):
                 continue
-
             elif line.month == 2 and int(header[-2:]) in range(30, 32) and bissexto:
                 continue
-
+            
+            # criando o novo datetime com a compinacao da linha e do header
             datahora = pd.Timestamp(line.year, line.month, int(header[-2:]))
+            # pegando o valor acumulado de chuva praquele dia
             valor_chuva = chuva_diaria[chuva_diaria.index == line][header][0]
             ANA_serie_temporal = ANA_serie_temporal.append({'datahora':datahora,
                                                             'chuva':valor_chuva},
